e5ab9914cb8b2f0b05e9c3e1d542a0a770144246
Laura Gambarte <lauu.91@gmail.com>
2022-10-02 22:35:39 +0200

    Created class Producto and subclasses

--- src/Producto.java
+++ src/Producto.java
@@ -14,4 +14,34 @@ public class Producto {
         this.nextId++;
         this.id = this.nextId;
     }
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
