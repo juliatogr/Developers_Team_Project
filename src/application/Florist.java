413feba4eb19259d92ae8723e8444704396c0699
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-10-10 12:46:05 +0200

    florist methods done

--- src/application/Florist.java
+++ src/application/Florist.java
@@ -1,6 +1,7 @@
 package application;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.ListIterator;
 
 public class Florist {
 	private String name;
@@ -41,21 +42,60 @@ public class Florist {
 	
 	
 	public void printStock() {
+		
+		for (Product p : stock) {
+			System.out.println(p);
+		}
 	};
 	
 	public void printStockQuantities() {
-	};
-	
-	public void printTotalValue() {
+		ArrayList<Product> stockCopy = (ArrayList<Product>) stock.clone(); 
+		
+		ListIterator<Product> stockIt = stockCopy.listIterator();
+		
+		while (stockIt.hasNext()) {
+			
+			
+			String name = stockIt.next().getName();
+			int quantity = 1;
+			
+			while (stockIt.hasNext()) {
+				
+				String name2 = stockIt.next().getName();
+				
+				if (name.equalsIgnoreCase(name2)) {
+					quantity++;
+					stockIt.remove();
+				}
+				
+				
+			}
+			System.out.println(name + ": " + quantity);
+			stockIt.remove();
+			
+		}
 	};
 	
 	public void createTicket() {
+		this.tickets.add(new Ticket());
+		
 	};
 	
 	public void printTickets() {
+		
+		for (Ticket t : tickets) {
+			System.out.println(t);
+		}
 	};
 	
-	public void orintTotalGain() {
+	public void printTotalGain() {
+		int totalValue = 0;
+		
+		for (Product p : stock) {
+			totalValue += p.getQuantity();
+		}
+		
+		System.out.println("Total value of the florist: " + totalValue + " €");
 	};
 
 	
